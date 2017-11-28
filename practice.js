d36da48c9f7be11d13de9b85cc2c949c2a35e7a4
James Carlson <carlson.james@gmail.com>
2017-11-28 10:37:39 -0700

    completed required

--- practice.js
+++ practice.js
@@ -6,9 +6,13 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+//Code here
+let me = {
+  name: "James",
+  age: 30
+}
 
-
+alert(me.name);
 
 
 
@@ -21,6 +25,14 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
   //Code here
+  let favoriteThings = {
+    band: "Mae",
+    food: "All",
+    person: "Taryn",
+    book: "Many",
+    movie: "Some",
+    holiday: "Thanksgiving"
+  }
 
 
 
@@ -31,7 +43,8 @@
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Code here
-
+  favoriteThings.car = "Diablo";
+  favoriteThings.brand = "Kirkland Signature";
 
 
 
@@ -39,7 +52,8 @@
 //Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 
   //Code here
-
+  favoriteThings.food = "Chicken Nuggets"
+  favoriteThings.book = "Harry Potter"
 
 
 
@@ -59,8 +73,10 @@ that is named color, with the value being the color of your backpack.
 */
 
   //Code here
-
-
+let backPack = {}
+var item = 'firstPocket';
+backPack[item] = 'chapstick';
+backPack.color = "black";
 
 
 
@@ -68,7 +84,7 @@ that is named color, with the value being the color of your backpack.
 //After you do the above, alert your entire backPack object.
 
   //Code here
-
+alert(backPack);
  
 
 /*
@@ -78,7 +94,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
   //Code here
-
+console.log(backPack);
 
 
 
@@ -100,6 +116,8 @@ var user2 = {
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
   //Code Here
+  user2.name = 'Tyler S. McGinnis'
+  user2.email = 'tyler.mcginnis@devmounta.in'
 
 
 
